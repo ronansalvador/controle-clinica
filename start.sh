7c138a95ca58e62a23558ea7d4719023995672ba
Ronan Salvador <ronansalvador@yahoo.com.br>
2023-05-29 16:09:07 -0300

    feat: configuração de instalação

--- start.sh
+++ start.sh
@@ -2,9 +2,10 @@
 
 # acessar pasta backend e executar npm run dev
 cd backend
-npm install &
+npm run db:reset
+npm run dev &
 cd ..
 
 # acessar pasta frontend e executar npm start
 cd frontend
-npm install
+npm run start
